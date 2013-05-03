244818136a2cea8753819223ef85d75e3ffe0087
Grzegorz Gajoch <gajoch@gmail.com>
2013-05-04 01:43:11 +0200

    Have to more add orientations to 4 and 5 type

--- tetris.cpp
+++ tetris.cpp
@@ -50,30 +50,24 @@ const int INF = 1000000001;
 #define COLUMNS 7
 
 
-struct ScreenField
-{
-	uint8_t value;
-	uint8_t blockNr;
-};
-
-ScreenField screen[ROWS][COLUMNS+1];
-
+bool screen[ROWS][COLUMNS];
 
 struct Block
 {
-        uint8_t X1 : 4; //X. == 8 when inactive
-	uint8_t X2 : 4;
-	uint8_t X3 : 4;
-	uint8_t X4 : 4;
+        uint8_t X1 : 3;
+	uint8_t X2 : 3;
+	uint8_t X3 : 3;
+	uint8_t X4 : 3;
 
 	uint8_t Y1 : 4;
 	uint8_t Y2 : 4;
 	uint8_t Y3 : 4;
 	uint8_t Y4 : 4;
+	
+	uint8_t type : 3;
+	uint8_t orient : 1;
 
-	uint8_t locked : 1;
-
-	Block(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, uint8_t x3, uint8_t y3, uint8_t x4, uint8_t y4)
+	Block(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, uint8_t x3, uint8_t y3, uint8_t x4, uint8_t y4, uint8_t orient, uint8_t type)
 	{
 		X1 = x1;
 		X2 = x2;
@@ -83,18 +77,183 @@ struct Block
 		Y2 = y2;
 		Y3 = y3;
 		Y4 = y4;
-		locked = 0;
+		this->type = type;
+		this->orient = orient;
 	}
 	Block()
 	{
-		X1 = X2 = X3 = X4 = Y1 = Y2 = Y3 = Y4 = locked = 0;
+		X1 = X2 = X3 = X4 = Y1 = Y2 = Y3 = Y4 = type = orient = 0;
 	}
 };
 
 
-Block blocks[20];
-Block *activeBlock;
-uint8_t numberOfBlocks = 0;
+Block activeBlock;
+
+
+#define SCREEN(x) (screen[activeBlock.Y ## x ][activeBlock.X ## x ])
+#define ALLBLOCK SCREEN(1) = SCREEN(2) = SCREEN(3) = SCREEN(4)
+
+
+
+void setActiveBlock()
+{
+	ALLBLOCK = true;
+}
+void unsetActiveBlock()
+{
+	ALLBLOCK = false;
+}
+#define FORALLELEMENTS(a) ( a (1) || a (2) || a (3) ||  a (4) )
+
+#define XACTIVEBLOCKCOORS(a) (activeBlock.X ## a) 
+#define TOUCHLEFT(a) (XACTIVEBLOCKCOORS(a) == 0)
+#define TOUCHRIGHT(a) (XACTIVEBLOCKCOORS(a) == (COLUMNS-1))
+#define SCREENATRIGHT(a) (screen[activeBlock.Y ## a ][activeBlock.X ## a + 1])
+#define SCREENATLEFT(a) (screen[activeBlock.Y ## a ][activeBlock.X ## a - 1])
+
+void shiftActiveBlockLeft()
+{
+	unsetActiveBlock();
+	if( FORALLELEMENTS(TOUCHLEFT) || FORALLELEMENTS(SCREENATLEFT) ) return;
+	--XACTIVEBLOCKCOORS(1);
+	--XACTIVEBLOCKCOORS(2);
+	--XACTIVEBLOCKCOORS(3);
+	--XACTIVEBLOCKCOORS(4);
+	setActiveBlock();
+}
+
+void shiftActiveBlockRight()
+{
+	unsetActiveBlock();
+	if( FORALLELEMENTS(TOUCHRIGHT) || FORALLELEMENTS(SCREENATRIGHT) ) return;
+	++XACTIVEBLOCKCOORS(1);
+	++XACTIVEBLOCKCOORS(2);
+	++XACTIVEBLOCKCOORS(3);
+	++XACTIVEBLOCKCOORS(4);
+	setActiveBlock();
+}
+
+
+#define UNDERACTIVEBLOCK(x) (screen[activeBlock.Y ## x+1][activeBlock.X ## x ])
+#define ZEROY(x) (activeBlock.Y ## x == (ROWS-1))
+
+bool shiftActiveBlockDown()
+{
+	if( UNDERACTIVEBLOCK(1) || UNDERACTIVEBLOCK(2) || UNDERACTIVEBLOCK(3) || UNDERACTIVEBLOCK(4) || ZEROY(1) || ZEROY(2) || ZEROY(3) || ZEROY(4) ) return false;
+	++activeBlock.Y1;
+	++activeBlock.Y2;
+	++activeBlock.Y3;
+	++activeBlock.Y4;
+	return true;
+}
+
+#define X(a) (tempBlock.X ## a)
+#define Y(a) (tempBlock.Y ## a)
+#define TAKEN(x) (screen[Y(x)][X(x)])
+#define ANYTAKEN() (TAKEN(1) || TAKEN(2) || TAKEN(3) || TAKEN(4))
+
+#define SHIFTORIENT() (activeBlock.orient = !activeBlock.orient)
+
+bool rotateActiveBlock()
+{
+	Block tempBlock = activeBlock;
+	unsetActiveBlock();
+	printf("(%d)\n",activeBlock.type);
+	if( activeBlock.type == 1 )
+	{
+		if( activeBlock.orient == 0)
+		{
+			if( screen[Y(2)][X(2)-1] || screen[Y(2)][X(2)+1] || screen[Y(2)][X(2)+2] || X(2) < 1 || X(2) +2 >= COLUMNS ) return false;
+			Y(1) = Y(3) = Y(4) = Y(2);
+			X(1) = X(2)-1;
+			X(3) = X(2)+1;
+			X(4) = X(2)+2;
+		}
+		else
+		{
+			if( screen[Y(2)-1][X(2)] || screen[Y(2)+1][X(2)] || screen[Y(2)+2][X(2)] || Y(2) == 0 || Y(2) + 2 >= ROWS ) return false;
+			X(1) = X(3) = X(4) = X(2);
+			Y(1) = Y(2)-1;
+			Y(3) = Y(2)+1;
+			Y(4) = Y(2)+2;
+		}
+	}
+	else if( activeBlock.type == 2 )
+	{
+		if( activeBlock.orient == 0 )
+		{
+			X(1) = X(4);
+			Y(1) = Y(2)-1;
+			--Y(4);
+
+		}
+		else
+		{
+			if( X(2) == 0 ) return false;
+			X(1) = X(2)-1;
+			++Y(1);
+			++Y(4);
+		}
+	}
+	else if( activeBlock.type == 3 )
+	{
+		if( activeBlock.orient == 0 )
+		{
+			++Y(2);
+			X(3) = X(2);
+			Y(3) = Y(2)+1;
+		}
+		else
+		{
+			if( Y(2) == 0 ) return false;
+			--Y(2);
+			Y(3) = Y(4);
+			X(3) = X(4)-1;
+		}
+	}
+	else if( activeBlock.type == 4 )
+	{
+		if( activeBlock.orient == 0 )
+		{
+			if( Y(2)+2 >= ROWS ) return false;
+			X(3) = X(4) = X(2);
+			Y(3) = Y(2)+1;
+			Y(4) = Y(3)+1;
+		}
+		else
+		{
+			if( X(2) + 1 >= COLUMNS ) return false;
+			X(3) = X(2)+1;
+			Y(3) = Y(2);
+			X(4) = X(1);
+			Y(4) = Y(1)+1;
+		}
+	}
+	else if( activeBlock.type == 5 )
+	{
+		if( activeBlock.orient == 0 )
+		{
+			if(X(2) == 0 ) return false;
+			Y(1) = Y(2);
+			X(1) = X(2)-1;
+			++X(4);
+		}
+		else
+		{
+			--X(4);
+			X(1) = X(2);
+			Y(1) = Y(4)+1;
+		}
+	}
+	if( ANYTAKEN() ) return false;
+	else
+	{
+		activeBlock = tempBlock;
+	}
+	SHIFTORIENT();
+	setActiveBlock();
+	return true;
+}
 
 bool generate_new_block()
 {
@@ -103,30 +262,40 @@ bool generate_new_block()
 	switch(ran)
 	{
 	case 0:
-		a = Block(1,0,2,0,3,0,4,0);
+//OO
+//OO
+		a = Block(3,0,4,0,3,1,4,1,0,0); 
 		break;
 	case 1:
-		a = Block(2,0,3,0,2,1,2,2);
+//OOOO
+		a = Block(1,0,2,0,3,0,4,0,1,1);
 		break;
 	case 2:
-		a = Block(2,0,3,0,3,1,3,2);
+//OO
+// OO
+		a = Block(3,0,4,0,4,1,5,1,0,2); 
 		break;
 	case 3:
-		a = Block(2,0,3,0,2,1,3,1);
+// OO
+//OO
+		a = Block(4,0,5,0,3,1,4,1,0,3);
 		break;
 	case 4:
-		a = Block(2,0,3,0,3,1,4,1);
+//OOO
+//O
+		a = Block(2,0,3,0,4,0,2,1,1,4);
 		break;
 	case 5:
-		a = Block(3,0,4,0,2,1,3,1);
+//OOO
+//  O
+		a = Block(2,0,3,0,4,0,4,1,1,5);
 		break;
 	}
 
-#define FIELD(x) (screen[a.Y ## x][a.X ## x].value)
+#define FIELD(x) (screen[a.Y ## x][a.X ## x])
 
 	if( FIELD(1) || FIELD(2) || FIELD(3) || FIELD(4) ) return 0;
-	blocks[numberOfBlocks] = a;
-	activeBlock = &(blocks[numberOfBlocks]);
+	activeBlock = a;
 }
 
 
@@ -143,7 +312,7 @@ void print_screen()
 		printf("|");
 		REP(x,COLUMNS)
 		{
-			if( screen[y][x].value ) printf("0");
+			if( screen[y][x] ) printf("0");
 			else printf(" ");
 		}
 		printf("|\n");
@@ -155,93 +324,32 @@ void print_screen()
 	printf("\n");
 }
 
-
-#define COORDS(x) screen[blocks[index].Y ## x+1][blocks[index].X ## x]
-
-#define CHECK_VALUE(x) 	(COORDS(x).value && blocks[COORDS(x).blockNr].locked )
-
-bool mark_if_locked(uint8_t index)
-{
-	if( CHECK_VALUE(1) || CHECK_VALUE(2)|| CHECK_VALUE(3) || CHECK_VALUE(4) || blocks[index].Y1 == 14 || blocks[index].Y2 == 14 || blocks[index].Y3 == 14 || blocks[index].Y4 == 14 )
-	{
-		return true;
-	}
-	return false;
-}
-
-
 void clear_screen() 
 {
 	for(uint8_t y = 0; y < ROWS; ++y)
 	{
 		for(uint8_t x = 0; x < COLUMNS; ++x)
 		{
-			screen[y][x].value = 0;
+			screen[y][x] = 0;
 		}
 	}
 }
-int8_t newElementIndex = -1;
-
+void checkForLines();
 void update_screen()
 {
-	for(uint8_t i = 1; i <= numberOfBlocks; ++i)
+	unsetActiveBlock();
+	if( shiftActiveBlockDown() == false )
 	{
-		if( ! blocks[i].locked )
-		{
-			++blocks[i].Y1;
-			++blocks[i].Y2;
-			++blocks[i].Y3;
-			++blocks[i].Y4;
-			if( mark_if_locked(i) )
-			{
-				blocks[i].locked = true;
-				newElementIndex = numberOfBlocks+1;
-			}
-		}
+		setActiveBlock();
+		generate_new_block();
+		setActiveBlock();
 	}
-	if( newElementIndex != -1 )
-	{
-		numberOfBlocks = newElementIndex;
-		newElementIndex = -1;
-		if( !  generate_new_block() )
-		{
-			exit(0);
-		}
-	}
-	clear_screen();
-
-	for(uint8_t i = 1; i <= numberOfBlocks; ++i)
-	{
-		screen[blocks[i].Y1][blocks[i].X1].value = 1;
-		screen[blocks[i].Y2][blocks[i].X2].value = 1;		
-		screen[blocks[i].Y3][blocks[i].X3].value = 1;		
-		screen[blocks[i].Y4][blocks[i].X4].value = 1;
-		screen[blocks[i].Y1][blocks[i].X1].blockNr = screen[blocks[i].Y2][blocks[i].X2].blockNr = screen[blocks[i].Y3][blocks[i].X3].blockNr = screen[blocks[i].Y4][blocks[i].X4].blockNr = i;
-	}
+	checkForLines();
+	setActiveBlock();
 }
 
 
-#define XACTIVECOORS(a) (activeBlock->X ## a) 
-#define TOUCHLEFT(a) (XACTIVECOORS(a) == 0)
-#define TOUCHRIGHT(a) (XACTIVECOORS(a) == (COLUMNS-1))
 
-void shiftActiveLeft()
-{
-	if( activeBlock->locked || TOUCHLEFT(1) || TOUCHLEFT(2) || TOUCHLEFT(3) || TOUCHLEFT(4) ) return;
-	--XACTIVECOORS(1);
-	--XACTIVECOORS(2);
-	--XACTIVECOORS(3);
-	--XACTIVECOORS(4);
-}
-
-void shiftActiveRight()
-{
-	if( activeBlock->locked || TOUCHRIGHT(1) || TOUCHRIGHT(2) || TOUCHRIGHT(3) || TOUCHRIGHT(4) ) return;
-	++XACTIVECOORS(1);
-	++XACTIVECOORS(2);
-	++XACTIVECOORS(3);
-	++XACTIVECOORS(4);
-}
 
 
 void checkForLines()
@@ -251,23 +359,30 @@ void checkForLines()
 		bool line = true;
 		for(uint8_t X = 0; X < COLUMNS; ++X)
 		{
-			if( board[Y][X].active == false )
+			if( screen[Y][X] == false )
 			{
+				line = false;
 				break;
 			}
 		}
 		if( line )
 		{
-
+			for(uint8_t a = Y; a > 1; --a)
+			{
+				for(uint8_t X = 0; X < COLUMNS; ++X)
+				{
+					screen[a][X] = screen[a-1][X];
+				}
+			}
 		}
 	}
 }
 
 int main(int argc, char **argv)
 {
-	numberOfBlocks = 0;
-	newElementIndex = true;;
-
+	srand(time(NULL));
+	generate_new_block();
+	setActiveBlock();
 	while(1)
 	{
 		update_screen();
@@ -277,9 +392,10 @@ int main(int argc, char **argv)
 		while( true )
 		{
 			scanf("%c",&x);
-			if( x == '.' ) shiftActiveRight();
-			else if( x == ',' ) shiftActiveLeft();
-			else if( x == '/' ) {}
+			if( x == '.' ) shiftActiveBlockRight();
+			else if( x == ',' ) shiftActiveBlockLeft();
+			else if( x == '/' ) rotateActiveBlock();
+			else if( x == '\'') {}
 			else continue;
 			break;
 		}
